63395d1343067fa4d8804b5fea36a919f321512d
liuyj <liu.yj1998@outlook.com>
2022-03-04 17:29:18 +0800

    study 1

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+const http = require('http');
+const https = require('https');
 const Koa = require('koa');
 const app = new Koa();
+http.createServer(app.callback()).listen(3000);
+https.createServer(app.callback()).listen(3001);
+// x-response-time
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
 
 app.use(async ctx => {
     ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+app.context.db = db();
+
+app.use(async ctx => {
+    console.log(ctx.db);
+});
+
+app.on('error', (err, ctx) => {
+    log.error('server error', err, ctx)
+});
+// app.listen(3000);
